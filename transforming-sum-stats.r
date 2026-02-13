0772e3b9a034a47da82b5c219c4198c7987f2361
Haileyd78 <hailey.davis@radboudumc.nl>
2026-02-13 12:22:55 +0100

    Update transforming-sum-stats.r

--- transforming-sum-stats.r
+++ transforming-sum-stats.r
@@ -3,6 +3,8 @@ dat <- read.table("ADHD.QC4.meta", header=TRUE, stringsAsFactors=FALSE)
 
 # Convert odds ratio to beta
 dat$BETA <- log(dat$OR)
+dat$SE <- dat$SE / dat$OR
+
 
 # Saving transformed summary statistics 
 write.table(dat, "ADHD.Transformed.meta", quote=F, row.names=F, sep="\t")
